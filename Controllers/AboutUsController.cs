6d397fc714487c096cf9b8fa3961de297c665f86
muhdnaufal22 <muhdnaufalyatim21@gmail.com>
2025-03-19 08:57:16 +0800

    Add files via upload

---
+++ Controllers/AboutUsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using CustomerManagementAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CustomerManagementAPI.Controllers
+{
+    public class AboutUsController : Controller
+    {
+        private readonly ILogger<AboutUsController> _logger;
+
+        public AboutUsController(ILogger<AboutUsController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Data/CustomerManagementContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CustomerManagementAPI.Models;
+
+namespace CustomerManagementAPI.Data
+{
+    public class CustomerManagementContext : DbContext
+    {
+        public CustomerManagementContext(DbContextOptions<CustomerManagementContext> options) : base(options) { }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Booking> Bookings { get; set; }
+    }
+}
